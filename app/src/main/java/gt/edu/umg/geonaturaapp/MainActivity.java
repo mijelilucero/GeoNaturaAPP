a12f5f3837ad4beea9bf0675427993400d880a7f
Mijeli <mlucerob@miumg.edu.gt>
2024-10-29 17:55:14 -0600

    Initial commit

---
+++ app/src/main/java/gt/edu/umg/geonaturaapp/MainActivity.java
@@ -0,0 +1,39 @@
+package gt.edu.umg.geonaturaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnEmpezarApp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnEmpezarApp = findViewById(R.id.btnEmpezarApp);
+
+        btnEmpezarApp.setOnClickListener(v -> {
+            Toast.makeText(this, "Entrando a la App...", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
